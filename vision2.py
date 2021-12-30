71a7bbd5c6c4f36f50cbbb5b8f9612a03cd22eba
John Hogan <john@thehogans.name>
2021-12-30 16:00:45 -0500

    work1

--- vision2.py
+++ vision2.py
@@ -56,6 +56,7 @@ from queue import Queue
 import threading
 import pyray as ray
 import math
+from collections import Counter
 #from ctypes import *
 
 print(dai.__version__)
@@ -344,7 +345,7 @@ class VisionSystem:
                             label = labelMap[detection.label]
                         except:
                             label = detection.label
-                        self.trackedObjectManager.processDetection(detection, time.time())
+                    self.trackedObjectManager.processDetections(detections, time.time())  # done this way to easier know if multiple items exist
                         #print(f"{label} at {detection.spatialCoordinates.x:.2f}, {detection.spatialCoordinates.y:.2f}, {detection.spatialCoordinates.z:.2f}")
 
     def runRaylibDisplayThread(self):
@@ -641,9 +642,12 @@ class TrackedObjectManager:
     of the same label to be tracked.  It will accomplish this by tracking their locations
     and determine which detection belongs to which object.
     '''
+
+    
     def __init__(self):
         print("initializing tracked object manager")
         self.trackedObjects = []
+        self.trackedObjectsTracker = {}
         self.objectLocationThreshold = [
             40,               40,           200,              100,            10000,         5000,            6000,
             300,              400,         100,              300,            500,           200,             50,
@@ -659,36 +663,55 @@ class TrackedObjectManager:
             50,               50,           10,
         ]
 
-    def processDetection(self, detection, time):
-        candidate = None
-        maxDistance = 0
-        distance = -1
-        for object in self.trackedObjects:
-            if object.label == detection.label:
-                distanceToCurrent = self.computeDistanceOfDetectionFromObject(object.currentLocation, detection.spatialCoordinates, time )
-                distanceToPrevious = self.computeDistanceOfDetectionFromObject(object.previousLocation, detection.spatialCoordinates, time )
-                distance = distanceToCurrent if distanceToCurrent<distanceToPrevious else distanceToPrevious
-                if distance < self.objectLocationThreshold[detection.label]:
-                    if candidate is not None:
-                        if candidate[1] > distance:  #if further away
-                            candidate = (object, distance)
-                    else:
-                        candidate = (object, distance)
-                else:
-                    if distance > maxDistance:
-                        maxDistance = distance
-        if candidate is None: #no matching objects found, so create new object
-            newObject = TrackedObject(detection.label,detection.spatialCoordinates, time)
-            self.trackedObjects.append(newObject)
-            print(f"new {newObject.getName()} being tracked due to distance = {maxDistance:0.2f}")
-            print(f"{detection.spatialCoordinates.x:0.2f}, {detection.spatialCoordinates.y:0.2f}, {detection.spatialCoordinates.z:0.2f}")
-        else:
-            candidate[0].updateLocation(detection.spatialCoordinates, time)
-            print(f"{candidate[0].getName()} position updated, ({detection.spatialCoordinates.x:0.2f}, {detection.spatialCoordinates.y:0.2f}, {detection.spatialCoordinates.z:0.2f}) distance = {candidate[1]:0.2f}.")
+    def processDetections(self, detections, time):
+        #determine how many of each label exists in frame and if quantity is less than what's tracked, create a new one
+        labels = []
+        for detection in detections:
+            labels.append(detection.label)
+        objects = Counter(labels)
+        for object in objects:
+            if object in self.trackedObjectsTracker:
+                count = self.trackedObjectsTracker[object]["count"]
+                if count < objects[object]:
+                    newObject = TrackedObject(object, None, 0)
+                    self.trackedObjects.append(newObject)
+                    self.addNewTrackedObjectToTracker(newObject)
+                    print(f"new object {newObject.name} tracked")
+            else:
+                for x in range(objects[object]):
+                    newObject = TrackedObject(object, None, 0)
+                    self.trackedObjects.append(newObject)
+                    self.addNewTrackedObjectToTracker(newObject)
+                    print(f"new object {newObject.name} tracked")
+                    
+        for detection in detections:
+            candidate = None
+            minDistance = 99999
+            distance = -1
+            for object in self.trackedObjects:
+                if object.label == detection.label:
+                    distanceToCurrent = self.computeDistanceOfDetectionFromObject(object.currentLocation, detection.spatialCoordinates, time )
+                    distanceToPrevious = self.computeDistanceOfDetectionFromObject(object.previousLocation, detection.spatialCoordinates, time )
+                    distance = distanceToCurrent if distanceToCurrent<distanceToPrevious else distanceToPrevious
+                    if distance < minDistance:
+                        minDistance = distance
+                        candidate = object
+            candidate.updateLocation(detection.spatialCoordinates, time)
+            print(f"{candidate.getName()} position updated, ({detection.spatialCoordinates.x:0.2f}, {detection.spatialCoordinates.y:0.2f}, {detection.spatialCoordinates.z:0.2f}) distance = {minDistance}.")
 
     def computeDistanceOfDetectionFromObject(self, l, c, t):
-        return math.sqrt( (l[0]-c.x)**2 + (l[1]-c.y)**2 + (l[2]-c.z)**2 ) / (t-l[3]) / 100 #scaling factor
+        if l[3]>0:
+            return math.sqrt( (l[0]-c.x)**2 + (l[1]-c.y)**2 + (l[2]-c.z)**2 ) / (t-l[3]) / 100 #scaling factor
+        else:
+            return 5000
 
+    def addNewTrackedObjectToTracker(self, newObject):
+        if newObject.label in self.trackedObjectsTracker:
+            self.trackedObjectsTracker[newObject.label]["count"]=self.trackedObjectsTracker[newObject.label]["count"]+1
+            return
+        self.trackedObjectsTracker[newObject.label]={"name":newObject.name, "count":1}
+                
+            
 class TrackedObject:
 
 
@@ -698,7 +721,7 @@ class TrackedObject:
         self.name = 0
         self.previousLocation = (0, 0, 0, 0)
         self.currentLocation = (0, 0, 0, 0)
-        self.currentVelocity = (0, 0, 0)
+        self.futureLocation = (0, 0, 0, 0)
 
     def __init__(self, label, c, t):
         self.ID = 0
@@ -707,12 +730,18 @@ class TrackedObject:
             self.name = labelMap[label]
         except:
             self.name = label
-        self.previousLocation = (c.x, c.y, c.z, t)
-        self.currentLocation = (c.x, c.y, c.z,t )
-        self.currentVelocity = (0, 0, 0)
+        if c is not None:
+            self.previousLocation = (c.x, c.y, c.z, t)
+            self.currentLocation = (c.x, c.y, c.z,t )
+            self.futureLocation = (c.x, c.y, c.z,t )
+        else:
+            self.previousLocation = (0, 0, 0, t)
+            self.currentLocation = (0, 0, 0, t)
+            self.futureLocation = (0, 0, 0, t)
+        
 
     def updateLocation(self, c, t):
-        #todo: add one euro filter
+        #todo: add one euro filter or kalman filter
         self.previousLocation = (self.currentLocation[0], self.currentLocation[1], self.currentLocation[2], self.currentLocation[3])
         self.currentLocation = (c.x, c.y, c.z, t)
 
